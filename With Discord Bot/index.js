94400211a078a4cfe70482267879046d1b41a943
ShreshthTiwari <68736752+ShreshthTiwari@users.noreply.github.com>
2022-01-20 14:38:09 +0530

    updates

---
+++ With Discord Bot/index.js
@@ -0,0 +1,69 @@
+require('dotenv').config();
+const {Client, Intents} = require('discord.js');
+require('dotenv').config();
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS, Intents.FLAGS.GUILD_WEBHOOKS, Intents.FLAGS.GUILD_INVITES, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_MESSAGE_TYPING, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.DIRECT_MESSAGE_REACTIONS, Intents.FLAGS.DIRECT_MESSAGE_TYPING, Intents.FLAGS.GUILD_SCHEDULED_EVENTS], partials: ["CHANNEL"] });
+
+const http = require("http");
+const fs = require("fs");
+let path = require('path');
+
+const config = require("./config.json");
+const PORT = config.webPort;
+const activity = config.activity;
+
+client.on("error", console.error);
+
+client.on('ready', async () => {
+  await client.user.setActivity(activity, {type: "WATCHING"});
+  console.log(`Logged in as: ${client.user.tag}`);
+});
+
+http.createServer(function (request, response) {
+  let filePath = '.' + request.url;
+  if (filePath == './') {
+    filePath = 'index.html';
+  }
+
+  let extname = String(path.extname(filePath)).toLowerCase();
+  let mimeTypes = {
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.css': 'text/css',
+    '.json': 'application/json',
+    '.png': 'image/png',
+    '.jpg': 'image/jpg',
+    '.gif': 'image/gif',
+    '.svg': 'image/svg+xml',
+    '.wav': 'audio/wav',
+    '.mp4': 'video/mp4',
+    '.woff': 'application/font-woff',
+    '.ttf': 'application/font-ttf',
+    '.eot': 'application/vnd.ms-fontobject',
+    '.otf': 'application/font-otf',
+    '.wasm': 'application/wasm'
+  };
+
+  let contentType = mimeTypes[extname] || 'application/octet-stream';
+  fs.readFile(filePath, function(error, content) {
+    if (error) {
+      if(error.code == 'ENOENT') {
+        fs.readFile('./404.html', function(error, content) {
+          response.writeHead(404, { 'Content-Type': 'text/html' });
+            response.end(content, 'utf-8');
+          });
+        }
+        else {
+          response.writeHead(500);
+          response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+        }
+      }
+      else {
+        response.writeHead(200, { 'Content-Type': contentType });
+        response.end(content, 'utf-8');
+      }
+  });
+
+}).listen(PORT);
+console.log(`\nWebsite running on Port: ${PORT}`);
+
+client.login();
